ffc60451b29a71278f15293400980fa4bbfb0196
Niklas <n11werthmann@gmail.com>
2025-02-12 10:56:55 +0100

    Fixed spelling mistake

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from app import create_app
 flask_app = create_app()
 
 if __name__ == "__main__":
-    #from waitress import xserve
+    #from waitress import serve
     #serve(flask_app, host="0.0.0.0", port=5000) # for prod
     flask_app.run(debug=True) #for development
