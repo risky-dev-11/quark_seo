c94ca83283fad1074664de62dade6949222fe082
risky <n11werthmann@gmail.com>
2025-03-26 01:16:15 +0100

    Updated Card Builder to the structure defined in the Studienarbeit. Added Core Web Vitals, etc.

--- card_builders.py
+++ card_builders.py
@@ -12,35 +12,77 @@ import requests
 import socket
 from langdetect import detect_langs, DetectorFactory
 import asyncio
+import dotenv
+import os
+
+dotenv.load_dotenv() 
 
 def build_all_cards(results, soup, url, response):
     """
     Builds and adds all the card sections to the results.
     This function delegates to smaller helper functions for each card.
     """
+
+    # Core Web Vitals Card
+    core_web_vitals_card = build_core_web_vitals_card(url)
+    core_web_vitals_card.add_to_results(results)
+    
+    # Technical SEO Card
+    technicalseo_card = build_technicalseo_card(soup, url, response)
+    technicalseo_card.add_to_results(results)
+    
     # Metadata Card
     metadata_card = build_metadata_card(soup, url)
     metadata_card.add_to_results(results)
     
-    # Page Quality Card
-    pagequality_card = build_pagequality_card(soup)
-    pagequality_card.add_to_results(results)
-    
-    # Page Structure Card
-    pagestructure_card = build_pagestructure_card(soup)
-    pagestructure_card.add_to_results(results)
-    
-    # Links Card
-    links_card = build_links_card(soup, url)
-    links_card.add_to_results(results)
-    
-    # Server Card
-    server_card = build_server_card(soup, url, response)
-    server_card.add_to_results(results)
-    
     # AI Analysis Card
     ai_results_card = build_ai_card(soup)
     ai_results_card.add_to_results(results)
+
+    # Page Quality Card
+    pagequality_and_links_card = build_pagequality_and_links_card(soup, url)
+    pagequality_and_links_card.add_to_results(results)
+    
+    
+def build_core_web_vitals_card(url):
+    
+    card = Card('Core Web Vitals')
+
+    api_key = os.getenv("GOOGLE_PAGESPEED_API_KEY")
+    
+    params = {
+        "url": url,
+        "key": api_key,
+        "strategy": "desktop"
+    }
+
+    response = requests.get('https://www.googleapis.com/pagespeedonline/v5/runPagespeed', params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        lighthouse_metrics = data.get("lighthouseResult", {}).get("audits", {})
+        lcp_category = Category('Largest Contentful Paint')
+        fcp_category = Category('First Contentful Paint')
+        tbt_category = Category('Total Blocking Time')
+        cls_category = Category('Cumulative Layout Shift')
+        speed_index_category = Category('Speed Index')
+
+        lcp_category.add_content(float(lighthouse_metrics.get("largest-contentful-paint", {}).get("numericValue", float('inf'))) / 1000 <= 2.5, f"Die Ladezeit der größten sichtbaren Komponente auf der Seite beträgt {lighthouse_metrics.get('largest-contentful-paint', {}).get('displayValue', 'N/A')} Sekunden.")
+        fcp_category.add_content(float(lighthouse_metrics.get("first-contentful-paint", {}).get("numericValue", float('inf'))) / 1000 <= 1.8, f"Die Ladezeit der ersten sichtbaren Komponente auf der Seite beträgt {lighthouse_metrics.get('first-contentful-paint', {}).get('displayValue', 'N/A')} Sekunden.")
+        tbt_category.add_content(float(lighthouse_metrics.get("total-blocking-time", {}).get("numericValue", float('inf'))) <= 200, f"Die Zeit, die durch blockierende Ressourcen verloren geht, beträgt {lighthouse_metrics.get('total-blocking-time', {}).get('displayValue', 'N/A')} Millisekunden.")
+        cls_category.add_content(float(lighthouse_metrics.get("cumulative-layout-shift", {}).get("numericValue", float('inf'))) <= 0.1, f"Die visuelle Stabilität der Seite während des Ladens beträgt {lighthouse_metrics.get('cumulative-layout-shift', {}).get('displayValue', 'N/A')}.")
+        speed_index_category.add_content(float(lighthouse_metrics.get("speed-index", {}).get("numericValue", float('inf'))) / 1000 <= 4.3, f"Die Geschwindigkeit, mit der der sichtbare Inhalt geladen wird, beträgt {lighthouse_metrics.get('speed-index', {}).get('displayValue', 'N/A')} Sekunden.")
+        
+        card.add_category(lcp_category)
+        card.add_category(fcp_category)
+        card.add_category(tbt_category)
+        card.add_category(cls_category)
+        card.add_category(speed_index_category)
+    else:
+        error_category = Category('Error')
+        error_category.add_content(False, 'Es ist ein Fehler bei dem Abrufen der Core Web Vitals aufgetreten. Bitte entschuldigen Sie die Unannehmlichkeiten.')
+    
+    return card
 
 def build_metadata_card(soup, url):
     card = Card('Metadaten')
@@ -116,7 +158,31 @@ def build_metadata_card(soup, url):
     if detected_lang not in meta_lang:
         language_category.add_content("improvement", "Hinweis: Unser Spracherkennungssystem kann auch falsch liegen. Bitte stellen Sie sicher, dass die im Meta-Tag angegebene Sprache auch der tatsächlichen Sprache des Inhalts entspricht.")
     card.add_category(language_category)
-    
+
+    # Check miscellaneous meta tags charset, content-type, canonical, viewport
+    meta_tags_category = Category('Meta-Tags')
+
+    # Charset
+    charset_meta = soup.find('meta', attrs={'charset': True})
+    charset_present = charset_meta is not None
+    meta_tags_category.add_content(charset_present, f"Charset Meta-Tag vorhanden: {charset_meta['charset']}" if charset_present else "Charset Meta-Tag fehlt.")
+
+    # Content-Type
+    content_type_meta = soup.find('meta', attrs={'http-equiv': 'Content-Type'})
+    content_type_present = content_type_meta is not None
+    meta_tags_category.add_content(content_type_present, f"Content-Type Meta-Tag vorhanden: {content_type_meta['content']}" if content_type_present else "Content-Type Meta-Tag fehlt.")
+
+    # Canonical
+    canonical_meta = soup.find('link', attrs={'rel': 'canonical'})
+    canonical_present = canonical_meta is not None
+    meta_tags_category.add_content(canonical_present, f"Canonical Meta-Tag vorhanden: {canonical_meta['href']}" if canonical_present else "Canonical Meta-Tag fehlt.")
+
+    # Viewport
+    viewport_meta = soup.find('meta', attrs={'name': 'viewport'})
+    viewport_present = viewport_meta is not None
+    meta_tags_category.add_content(viewport_present, f"Viewport Meta-Tag vorhanden: {viewport_meta['content']}" if viewport_present else "Viewport Meta-Tag fehlt.")
+
+    card.add_category(meta_tags_category)
     # Favicon Category
     favicon_category = Category('Favicon')
     has_favicon = soup.find('link', attrs={'rel': 'icon'}) is not None
@@ -125,9 +191,17 @@ def build_metadata_card(soup, url):
     
     return card
 
-def build_pagequality_card(soup):
-    card = Card('Seitenqualität')
+def build_pagequality_and_links_card(soup, url):
+    card = Card('Inhalt und Verlinkung')
     
+    # Bilder Category
+    images_category = Category('Bilder')
+    missing_alts = sum(1 for img in soup.find_all('img') if not img.get('alt') or not img.get('alt').strip())
+    images_category.add_content(missing_alts == 0, get_alt_attributes_missing_text(missing_alts))
+    if missing_alts > 0:
+        images_category.add_content('improvement', "Fügen Sie Alt-Attribute zu allen Bildern hinzu! Sie verbessern damit die Barrierefreiheit Ihrer Website und helfen Suchmaschinen, den Bildinhalt zu verstehen. Das steigert sowohl die Nutzerfreundlichkeit als auch Ihr Ranking.")
+    card.add_category(images_category)
+
     # Inhalt Category
     content_category = Category('Inhalt')
     title_text = soup.title.string if soup.title and soup.title.string.strip() else ""
@@ -138,64 +212,48 @@ def build_pagequality_card(soup):
     content_category.add_content(word_count >= 300, get_content_length_comment(word_count))
     
     sentences = [sentence.strip() for tag in ['p', 'div', 'span', 'li'] 
-                 for element in soup.find_all(tag) 
-                 for sentence in element.get_text().split('.') if sentence.strip()]
+                    for element in soup.find_all(tag) 
+                    for sentence in element.get_text().split('.') if sentence.strip()]
     duplicates = len(sentences) != len(set(sentences))
     content_category.add_content(duplicates, get_duplicate_text(duplicates))
     card.add_category(content_category)
     
-    # Bilder Category
-    images_category = Category('Bilder')
-    missing_alts = sum(1 for img in soup.find_all('img') if not img.get('alt') or not img.get('alt').strip())
-    images_category.add_content(missing_alts == 0, get_alt_attributes_missing_text(missing_alts))
-    if missing_alts > 0:
-        images_category.add_content('improvement', "Fügen Sie Alt-Attribute zu allen Bildern hinzu! Sie verbessern damit die Barrierefreiheit Ihrer Website und helfen Suchmaschinen, den Bildinhalt zu verstehen. Das steigert sowohl die Nutzerfreundlichkeit als auch Ihr Ranking.")
-    card.add_category(images_category)
-    
-    return card
-
-def build_pagestructure_card(soup):
-    card = Card('Seitenstruktur')
-    
-    headings_category = Category('Überschriften')
-    headings = [tag.name for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])]
-    structure_ok = all(int(headings[i][1]) >= int(headings[i + 1][1]) - 1 for i in range(len(headings) - 1))
-    hierarchy_ok = all(f'h{i}' in headings for i in range(1, 7) if any(f'h{j}' in headings for j in range(i, 7)))
-    headings_category.add_content(soup.find('h1') is not None, get_h1_heading_text(soup.find('h1') is not None))
-    headings_category.add_content(structure_ok, get_structure_text(structure_ok))
-    headings_category.add_content(hierarchy_ok, get_hierarchy_text(hierarchy_ok))
-    card.add_category(headings_category)
-    
-    return card
-
-def build_links_card(soup, url):
-    card = Card('Links')
+    # Links Category
+    links_category = Category('Verlinkung')
     
     # Internal links
     internal_links = [link for link in soup.find_all('a', href=True) if url in link['href']]
-    internal_category = Category('Interne Links')
-    internal_category.add_content(all(len(link.text) < 30 for link in internal_links), get_internal_length_linktext_text(all(len(link.text) < 30 for link in internal_links)))
+    links_category.add_content(all(len(link.text) < 30 for link in internal_links), get_internal_length_linktext_text(all(len(link.text) < 30 for link in internal_links)))
     no_text_internal = sum(1 for link in internal_links if not link.text.strip()) > 0
-    internal_category.add_content(not no_text_internal, get_internal_no_linktext_text(no_text_internal))
+    links_category.add_content(not no_text_internal, get_internal_no_linktext_text(no_text_internal))
     duplicate_internal = len([link.text for link in internal_links]) != len(set([link.text for link in internal_links]))
-    internal_category.add_content(not duplicate_internal, get_internal_linktext_repetitions_text(duplicate_internal))
-    card.add_category(internal_category)
+    links_category.add_content(not duplicate_internal, get_internal_linktext_repetitions_text(duplicate_internal))
     
     # External links
     external_links = [link for link in soup.find_all('a', href=True) if url not in link['href']]
-    external_category = Category('Externe Links')
-    external_category.add_content(all(len(link.text) < 30 for link in external_links), get_external_length_linktext_text(all(len(link.text) < 30 for link in external_links)))
+    links_category.add_content(all(len(link.text) < 30 for link in external_links), get_external_length_linktext_text(all(len(link.text) < 30 for link in external_links)))
     no_text_external = sum(1 for link in external_links if not link.text.strip()) > 0
-    external_category.add_content(not no_text_external, get_external_no_linktext_text(no_text_external))
+    links_category.add_content(not no_text_external, get_external_no_linktext_text(no_text_external))
     duplicate_external = len([link.text for link in external_links]) != len(set([link.text for link in external_links]))
-    external_category.add_content(not duplicate_external, get_external_linktext_repetitions_text(duplicate_external))
-    card.add_category(external_category)
+    links_category.add_content(not duplicate_external, get_external_linktext_repetitions_text(duplicate_external))
+    
+    card.add_category(links_category)
     
     return card
 
-def build_server_card(soup, url, response):
+def build_technicalseo_card(soup, url, response):
     card = Card('Technische SEO')
     
+    # Check for image formats
+    images = soup.find_all('img')
+    has_jpg_or_png = any(img.get('src', '').lower().endswith(('.jpg', '.jpeg', '.png')) for img in images)
+    image_format_category = Category('Bildformate')
+    image_format_category.add_content(not has_jpg_or_png, "Es wurden Bilder im JPG- oder PNG-Format gefunden." if has_jpg_or_png else "Es wurden keine Bilder im JPG- oder PNG-Format gefunden.")
+    if has_jpg_or_png:
+        image_format_category.add_content("improvement", "Es wird empfohlen, moderne Bildformate wie WebP oder AVIF zu verwenden, um die Ladezeiten zu optimieren.")
+    card.add_category(image_format_category)
+
+
     # Extract the base domain
     base_domain = url.split('//')[-1].split('/')[0]
     print(base_domain)
@@ -309,11 +367,11 @@ def build_serp_preview(soup, url, response):
     serp_points = 0
 
     desc_length_px = round(len(description) * 6.11)
-    if 110 <= len(description) <= 165:
+    if 110 <= desc_length_px <= 165:
         serp_points += 25
-    if 100 <= len(description) <= 120:
+    if 100 <= desc_length_px <= 120:
         serp_points += 25
-    if 30 <= len(title_text) <= 60:
+    if 30 <= desc_length_px <= 60:
         serp_points += 50
     
     serp_preview = {
